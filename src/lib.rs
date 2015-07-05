9624310aa90b36815e6264db38527c8971bdb359
gwenn <gtreguier@gmail.com>
2015-07-05 08:55:35 +0200

    Add binding to sqlite3_stmt_busy.

--- src/lib.rs
+++ src/lib.rs
@@ -412,6 +412,11 @@ impl SqliteConnection {
     pub fn is_autocommit(&self) -> bool {
         self.db.borrow().is_autocommit()
     }
+
+    /// Determine if all associated prepared statements have been reset.
+    pub fn is_busy(&self) -> bool {
+        self.db.borrow().is_busy()
+    }
 }
 
 impl fmt::Debug for SqliteConnection {
@@ -556,6 +561,20 @@ impl InnerSqliteConnection {
 
     fn is_autocommit(&self) -> bool {
         unsafe{ ffi::sqlite3_get_autocommit(self.db()) != 0 }
+    }
+
+    fn is_busy(&self) -> bool {
+        let db = self.db();
+        unsafe {
+            let mut stmt = ffi::sqlite3_next_stmt(db, ptr::null_mut());
+            while !stmt.is_null() {
+                if ffi::sqlite3_stmt_busy(stmt) != 0 {
+                    return true
+                }
+                stmt = ffi::sqlite3_next_stmt(db, stmt);
+            }
+        }
+        return false
     }
 }
 
@@ -1118,5 +1137,23 @@ mod test {
     fn test_is_autocommit() {
         let db = checked_memory_handle();
         assert!(db.is_autocommit(), "autocommit expected to be active by default");
+    }
+
+    #[test]
+    fn test_is_busy() {
+        let db = checked_memory_handle();
+        assert!(!db.is_busy());
+        let mut stmt = db.prepare("PRAGMA schema_version").unwrap();
+        assert!(!db.is_busy());
+        {
+            let mut rows = stmt.query(&[]).unwrap();
+            assert!(!db.is_busy());
+            let row = rows.next();
+            assert!(db.is_busy());
+            assert!(row.is_some());
+        }
+        assert!(db.is_busy());
+        stmt.reset_if_needed();
+        assert!(!db.is_busy());
     }
 }
