8ad0147a160d4e6c7d170dd932e47eb89026ec0f
gwenn <gtreguier@gmail.com>
2020-01-26 17:40:49 +0100

    Add support to function flags
    Fix example

--- src/functions.rs
+++ src/functions.rs
@@ -10,41 +10,46 @@
 //!
 //! ```rust
 //! use regex::Regex;
-//! use rusqlite::{Connection, Error, Result, NO_PARAMS};
+//! use rusqlite::{Connection, Error, FunctionFlags, Result, NO_PARAMS};
 //!
 //! fn add_regexp_function(db: &Connection) -> Result<()> {
-//!     db.create_scalar_function("regexp", 2, true, move |ctx| {
-//!         assert_eq!(ctx.len(), 2, "called with unexpected number of arguments");
+//!     db.create_scalar_function(
+//!         "regexp",
+//!         2,
+//!         FunctionFlags::SQLITE_UTF8 | FunctionFlags::SQLITE_DETERMINISTIC,
+//!         move |ctx| {
+//!             assert_eq!(ctx.len(), 2, "called with unexpected number of arguments");
 //!
-//!         let saved_re: Option<&Regex> = ctx.get_aux(0)?;
-//!         let new_re = match saved_re {
-//!             None => {
-//!                 let s = ctx.get::<String>(0)?;
-//!                 match Regex::new(&s) {
-//!                     Ok(r) => Some(r),
-//!                     Err(err) => return Err(Error::UserFunctionError(Box::new(err))),
+//!             let saved_re: Option<&Regex> = ctx.get_aux(0)?;
+//!             let new_re = match saved_re {
+//!                 None => {
+//!                     let s = ctx.get::<String>(0)?;
+//!                     match Regex::new(&s) {
+//!                         Ok(r) => Some(r),
+//!                         Err(err) => return Err(Error::UserFunctionError(Box::new(err))),
+//!                     }
 //!                 }
+//!                 Some(_) => None,
+//!             };
+//!
+//!             let is_match = {
+//!                 let re = saved_re.unwrap_or_else(|| new_re.as_ref().unwrap());
+//!
+//!                 let text = ctx
+//!                     .get_raw(1)
+//!                     .as_str()
+//!                     .map_err(|e| Error::UserFunctionError(e.into()))?;
+//!
+//!                 re.is_match(text)
+//!             };
+//!
+//!             if let Some(re) = new_re {
+//!                 ctx.set_aux(0, re);
 //!             }
-//!             Some(_) => None,
-//!         };
 //!
-//!         let is_match = {
-//!             let re = saved_re.unwrap_or_else(|| new_re.as_ref().unwrap());
-//!
-//!             let text = ctx
-//!                 .get_raw(1)
-//!                 .as_str()
-//!                 .map_err(|e| Error::UserFunctionError(e.into()))?;
-//!
-//!             re.is_match(text)
-//!         };
-//!
-//!         if let Some(re) = new_re {
-//!             ctx.set_aux(0, re);
-//!         }
-//!
-//!         Ok(is_match)
-//!     })
+//!             Ok(is_match)
+//!         },
+//!     )
 //! }
 //!
 //! fn main() -> Result<()> {
