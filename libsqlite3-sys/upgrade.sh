325bfb6344fe94c595a34ab79cdfba6aa0e12d21
gwenn <gtreguier@gmail.com>
2020-02-15 12:36:02 +0100

    Generate all three bindgen files

--- libsqlite3-sys/upgrade.sh
+++ libsqlite3-sys/upgrade.sh
@@ -1,26 +1,40 @@
 SCRIPT_DIR=$(cd "$(dirname "$_")" && pwd)
 echo $SCRIPT_DIR
 cd $SCRIPT_DIR
+
 export SQLITE3_LIB_DIR=$SCRIPT_DIR/sqlite3
+export SQLITE3_INCLUDE_DIR=$SQLITE3_LIB_DIR
 
 # Download and extract amalgamation
 SQLITE=sqlite-amalgamation-3310000
-curl -O https://sqlite.org/2020/$SQLITE.zip
-unzip -p $SQLITE.zip $SQLITE/sqlite3.c > $SQLITE3_LIB_DIR/sqlite3.c
-unzip -p $SQLITE.zip $SQLITE/sqlite3.h > $SQLITE3_LIB_DIR/sqlite3.h
-unzip -p $SQLITE.zip $SQLITE/sqlite3ext.h > $SQLITE3_LIB_DIR/sqlite3ext.h
+curl -sSf -O https://sqlite.org/2020/$SQLITE.zip
+unzip -p $SQLITE.zip $SQLITE/sqlite3.c >$SQLITE3_LIB_DIR/sqlite3.c
+unzip -p $SQLITE.zip $SQLITE/sqlite3.h >$SQLITE3_LIB_DIR/sqlite3.h
+unzip -p $SQLITE.zip $SQLITE/sqlite3ext.h >$SQLITE3_LIB_DIR/sqlite3ext.h
 rm -f $SQLITE.zip
 
-# Regenerate bindgen file
-rm -f $SQLITE3_LIB_DIR/bindgen_bundled_version.rs
-export SQLITE3_INCLUDE_DIR=$SQLITE3_LIB_DIR
+# Regenerate bindgen files
 cargo update
-# Just to make sure there is only one bindgen.rs file in target dir
-find $SCRIPT_DIR/../target -type f -name bindgen.rs -exec rm {} \;
-cargo build --features "buildtime_bindgen" --no-default-features
-find $SCRIPT_DIR/../target -type f -name bindgen.rs -exec cp {} $SQLITE3_LIB_DIR/bindgen_bundled_version.rs \;
+
+function generate_bindgen_binding() {
+  features=$1
+  target_file=$2
+
+  rm -f $target_file
+  # Just to make sure there is only one bindgen.rs file in target dir
+  find $SCRIPT_DIR/../target -type f -name bindgen.rs -exec rm {} \;
+  cargo build --features "$features" --no-default-features
+  find $SCRIPT_DIR/../target -type f -name bindgen.rs -exec cp {} $target_file \;
+  # TODO rustfmt $target_file
+}
+
+generate_bindgen_binding "buildtime_bindgen" $SQLITE3_LIB_DIR/bindgen_bundled_version.rs
+generate_bindgen_binding "buildtime_bindgen,loadable_extension" $SQLITE3_LIB_DIR/bindgen_bundled_version-ext.rs
+generate_bindgen_binding "buildtime_bindgen,loadable_extension_embedded" $SQLITE3_LIB_DIR/bindgen_bundled_version-ext-embed.rs
+
 # Sanity check
 cd $SCRIPT_DIR/..
 cargo update
 cargo test --features "backup blob chrono functions limits load_extension serde_json trace vtab bundled"
+# TODO check loadable_extension / loadable_extension_embedded
 echo 'You should increment the version in libsqlite3-sys/Cargo.toml'
