df9a1b14e1dd943ed5503826ef18a7a615ddd8b2
gwenn <gtreguier@gmail.com>
2020-02-15 19:15:32 +0100

    Remove non_threadsafe feature

--- src/inner_connection.rs
+++ src/inner_connection.rs
@@ -375,7 +375,6 @@ rusqlite was built against SQLite {} but the runtime SQLite version is {}. To fi
 #[cfg(not(any(
     feature = "loadable_extension",
     feature = "loadable_extension_embedded",
-    feature = "non_threadsafe"
 )))]
 static SQLITE_INIT: Once = Once::new();
 pub static BYPASS_SQLITE_INIT: AtomicBool = AtomicBool::new(false);
@@ -383,13 +382,9 @@ pub static BYPASS_SQLITE_INIT: AtomicBool = AtomicBool::new(false);
 // threading mode checks are not possible when built as a loadable extension
 // since the sqlite3_threadsafe, sqlite3_config, and sqlite3_initialize
 // API calls are not available via the sqlite3_api_routines struct.
-// They are also not available when sqlite is built without threadsafe (e.g.
-// -DSQLITE_THREADSAFE=0) which is represented here by the "non_threadsafe"
-// feature.
 #[cfg(any(
     feature = "loadable_extension",
     feature = "loadable_extension_embedded",
-    feature = "non_threadsafe"
 ))]
 fn ensure_safe_sqlite_threading_mode() -> Result<()> {
     Ok(())
@@ -398,7 +393,6 @@ fn ensure_safe_sqlite_threading_mode() -> Result<()> {
 #[cfg(not(any(
     feature = "loadable_extension",
     feature = "loadable_extension_embedded",
-    feature = "non_threadsafe"
 )))]
 fn ensure_safe_sqlite_threading_mode() -> Result<()> {
     // Ensure SQLite was compiled in thredsafe mode.
