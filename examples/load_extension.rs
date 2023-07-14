d7f2d55bfbffdfbd260dd8dac448b0b0eb551dc8
gwenn <gtreguier@gmail.com>
2023-07-14 11:07:59 +0200

    Add example loading extension

---
+++ examples/load_extension.rs
@@ -0,0 +1,19 @@
+//! Ensure loadable_extension.rs works.
+
+use rusqlite::{Connection, Result};
+
+fn main() -> Result<()> {
+    let db = Connection::open_in_memory()?;
+
+    unsafe {
+        db.load_extension_enable()?;
+        db.load_extension("target/debug/examples/libloadable_extension", None)?;
+        db.load_extension_disable()?;
+    }
+
+    let str = db.query_row("SELECT rusqlite_test_function()", [], |row| {
+        row.get::<_, String>(0)
+    })?;
+    assert_eq!(&str, "Rusqlite extension loaded correctly!");
+    Ok(())
+}
