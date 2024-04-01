c1b66ed41d618a98c3c2b2d49564ddf5917224be
gwenn <gtreguier@gmail.com>
2024-04-01 09:30:11 +0200

    Add more tests

--- src/types/value.rs
+++ src/types/value.rs
@@ -140,3 +140,24 @@ impl Value {
         }
     }
 }
+
+#[cfg(test)]
+mod test {
+    use crate::types::Type;
+    use super::Value;
+
+    #[test]
+    fn from() {
+        assert_eq!(Value::from(3.14), Value::Real(3.14));
+        assert_eq!(Value::from(vec![0u8]), Value::Blob(vec![0u8]));
+    }
+
+    #[test]
+    fn data_type() {
+        assert_eq!(Value::Null.data_type(), Type::Null);
+        assert_eq!(Value::Integer(0).data_type(), Type::Integer);
+        assert_eq!(Value::Real(0.).data_type(), Type::Real);
+        assert_eq!(Value::Text("".to_owned()).data_type(), Type::Text);
+        assert_eq!(Value::Blob(vec![]).data_type(), Type::Blob);
+    }
+}
