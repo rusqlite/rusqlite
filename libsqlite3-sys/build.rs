50aaceb07deebcefa3e995feb626dbc1cba24a26
Niels Becker <aaqqws@googlemail.com>
2024-11-05 00:00:31 +0100

    Fix build for android x86_64
    Adds a temporary workaround for [an issue] with the Rust compiler and Android when
    compiling for x86_64 devices.
    
    The Android NDK used to include `libgcc` for unwind support (which is required by Rust
    among others). From NDK r23, `libgcc` is removed, replaced by LLVM's `libunwind`.
    However, `libgcc` was ambiently providing other compiler builtins, one of which we
    require: `__extenddftf2` for software floating-point emulation. This is used by SQLite
    (via the `rusqlite` crate), which defines a `LONGDOUBLE_TYPE` type as `long double`.
    
    Rust uses a `compiler-builtins` crate that does not provide `__extenddftf2` because
    [it involves floating-point types that are not supported by Rust][unsupported]. For
    some reason, they _do_ export this symbol for `aarch64-linux-android`, but they do not
    for `x86_64-linux-android`. Thus we run into a problem when trying to compile and run
    the SDK on an x86_64 emulator.
    
    The workaround comes from [this Mozilla PR]: we tell Cargo to statically link the
    builtins from the Clang runtime provided inside the NDK, to provide this symbol.
    
    [an issue]: https://github.com/rust-lang/rust/issues/109717
    [this Mozilla PR]:https://github.com/mozilla/application-services/pull/5442
    [unsupported]: https://github.com/rust-lang/compiler-builtins#unimplemented-functions
    
    This fix was copied from: https://github.com/Electric-Coin-Company/zcash-android-wallet-sdk/commit/1bf2f848373bee833a11fbb8cd9b570218ac71bb
    
    Fixes #1380
    
    Co-authored-by: Jack Grigg <jack@electriccoin.co>

--- libsqlite3-sys/build.rs
+++ libsqlite3-sys/build.rs
@@ -92,10 +92,14 @@ fn main() {
 ))]
 mod build_bundled {
     use std::env;
-    use std::ffi::OsString;
+    use std::ffi::{OsStr, OsString};
     use std::path::{Path, PathBuf};
+    use std::process::Command;
 
     use super::{is_compiler, win_target};
+
+    /// The Clang version that the NDK version pinned in `gradle.properties` should be using.
+    const ANDROID_NDK_CLANG_VERSION_FALLBACK: &str = "17";
 
     pub fn main(out_dir: &str, out_path: &Path) {
         let lib_name = super::lib_name();
@@ -224,6 +228,8 @@ mod build_bundled {
         // https://android.googlesource.com/platform/external/sqlite/+/2c8c9ae3b7e6f340a19a0001c2a889a211c9d8b2/dist/Android.mk
         if super::android_target() {
             cfg.flag("-DSQLITE_TEMP_STORE=3");
+
+            setup_x86_64_android_workaround();
         }
 
         if cfg!(feature = "with-asan") {
@@ -310,6 +316,96 @@ mod build_bundled {
     fn find_openssl_dir(_host: &str, _target: &str) -> Option<PathBuf> {
         let openssl_dir = env("OPENSSL_DIR");
         openssl_dir.map(PathBuf::from)
+    }
+
+    /// Adds a temporary workaround for [an issue] with the Rust compiler and Android when
+    /// compiling for x86_64 devices.
+    ///
+    /// The Android NDK used to include `libgcc` for unwind support (which is required by Rust
+    /// among others). From NDK r23, `libgcc` is removed, replaced by LLVM's `libunwind`.
+    /// However, `libgcc` was ambiently providing other compiler builtins, one of which we
+    /// require: `__extenddftf2` for software floating-point emulation. This is used by SQLite
+    /// (via the `rusqlite` crate), which defines a `LONGDOUBLE_TYPE` type as `long double`.
+    ///
+    /// Rust uses a `compiler-builtins` crate that does not provide `__extenddftf2` because
+    /// [it involves floating-point types that are not supported by Rust][unsupported]. For
+    /// some reason, they _do_ export this symbol for `aarch64-linux-android`, but they do not
+    /// for `x86_64-linux-android`. Thus we run into a problem when trying to compile and run
+    /// the SDK on an x86_64 emulator.
+    ///
+    /// The workaround comes from [this Mozilla PR]: we tell Cargo to statically link the
+    /// builtins from the Clang runtime provided inside the NDK, to provide this symbol.
+    ///
+    /// [an issue]: https://github.com/rust-lang/rust/issues/109717
+    /// [this Mozilla PR]:https://github.com/mozilla/application-services/pull/5442
+    /// [unsupported]: https://github.com/rust-lang/compiler-builtins#unimplemented-functions
+    fn setup_x86_64_android_workaround() {
+        let target_os = env::var("CARGO_CFG_TARGET_OS").expect("CARGO_CFG_TARGET_OS not set");
+        let target_arch = env::var("CARGO_CFG_TARGET_ARCH").expect("CARGO_CFG_TARGET_ARCH not set");
+
+        if target_arch == "x86_64" && target_os == "android" {
+            let cc = if let Some(cc) = env::var_os("RUST_ANDROID_GRADLE_CC") {
+                // We are building in the context of the `org.mozilla.rust-android-gradle`
+                // plugin, which knows where the NDK is and provides the Clang path.
+                PathBuf::from(cc)
+            } else {
+                // We are probably building directly on the CLI. Construct a path to Clang.
+                let android_ndk_home =
+                    env::var_os("ANDROID_NDK_HOME").expect("ANDROID_NDK_HOME not set");
+                let build_os = match env::consts::OS {
+                    "linux" => "linux",
+                    "macos" => "darwin",
+                    "windows" => "windows",
+                    _ => panic!(
+                        "Unsupported OS. You must use either Linux, MacOS or Windows to build the crate."
+                    ),
+                };
+
+                let mut cc = PathBuf::from(android_ndk_home);
+                cc.push("toolchains");
+                cc.push("llvm");
+                cc.push("prebuilt");
+                cc.push(format!("{build_os}-x86_64"));
+                cc.push("bin");
+                cc.push("clang");
+                cc.set_extension(env::consts::EXE_EXTENSION);
+                cc
+            };
+
+            let mut link_path = cc
+                .ancestors()
+                .nth(2)
+                .expect("path format is known")
+                .join("lib");
+            link_path.push("clang");
+            link_path.push(get_clang_version(&cc));
+            link_path.push("lib");
+            link_path.push("linux");
+
+            if link_path.exists() {
+                println!("cargo:rustc-link-search={}", link_path.display());
+                println!("cargo:rustc-link-lib=static=clang_rt.builtins-x86_64-android");
+            } else {
+                panic!("Path {} does not exist", link_path.display());
+            }
+        }
+    }
+
+    fn get_clang_version(cc: impl AsRef<OsStr>) -> String {
+        let clang_version_output = Command::new(cc)
+            .arg("--version")
+            .output()
+            .ok()
+            .and_then(|o| String::from_utf8(o.stdout).ok());
+        clang_version_output
+            .as_deref()
+            .and_then(|s| s.split_once("clang version "))
+            .and_then(|(_, s)| s.split_once('.'))
+            .map(|(major_version, _)| major_version)
+            // If we couldn't run Clang for some reason, default to the Clang version that the
+            // NDK version pinned in `gradle.properties` should be using.
+            .unwrap_or(ANDROID_NDK_CLANG_VERSION_FALLBACK)
+            .into()
     }
 }
 
